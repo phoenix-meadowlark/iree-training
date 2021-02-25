e154c5216950812113faed18edd3afb8a0de2dee
Phoenix Meadowlark <meadowlark@google.com>
2021-02-25 12:42:22 -0800

    Update tf.js benchmarks with results

--- tfjs_browser/script.js
+++ tfjs_browser/script.js
@@ -1,99 +1,209 @@
+// Experiment Parameters
+const TFJS_BACKEND = "wasm";
+
+const BENCHMARK_ITERATIONS = 1000;
+const MS_BETWEEN_ITERATIONS = 0;
+
+const BENCHMARK_PREDICT = false;
+const BENCHMARK_FIT = false;
+const BENCHMARK_TRAIN_ON_BATCH = true;
+
+const MODEL_NAME = "mnist_dnn";
+const BATCH_SIZE = 32;
+const OPTIMIZER = "rmsprop";
+const LEARNING_RATE = 0.01; // Should have not effect.
+const OPTIMIZERS = {
+  sgd: tf.train.sgd(LEARNING_RATE),
+  adadelta: tf.train.adadelta(LEARNING_RATE),
+  adagrad: tf.train.adagrad(LEARNING_RATE),
+  adam: tf.train.adam(LEARNING_RATE),
+  adamax: tf.train.adamax(LEARNING_RATE),
+  momentum: tf.train.momentum(LEARNING_RATE, 0.1),
+  rmsprop: tf.train.rmsprop(LEARNING_RATE),
+}
+
+console.log("model_name:            " + MODEL_NAME);
+console.log("batch_size:            " + BATCH_SIZE);
+console.log("optimizer:             " + OPTIMIZER);
+console.log("benchmark_iterations:  " + BENCHMARK_ITERATIONS);
+console.log("ms_between_iterations: " + MS_BETWEEN_ITERATIONS);
+
+
+function get_compilation_options() {
+  return {
+    optimizer: OPTIMIZERS[OPTIMIZER],
+    loss: "categoricalCrossentropy",
+  };
+};
+
+
 function sleep(ms) {
   // https://stackoverflow.com/a/39914235
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-tf.setBackend('cpu').then(async () => {
-  console.log(tf.getBackend());
 
-  const BENCHMARK_ITERATIONS = 10;
+async function timeModelPredict(model, images) {
+  // don't time the jit compilation.
+  console.log("compiling model.predict");
+  const warmup = await model.predict(images);
+  warmup.dispose();
 
-  const EPOCHS = 1;
-  const BATCH_SIZE = 32;
-  const IMAGE_SIZE = [224, 224, 3]
-  const NUM_CLASSES = 1000
+  console.log("benchmarking model.predict");
+  let times = [];
+  for (let i = 0; i < BENCHMARK_ITERATIONS; i++) {
+    let startTime = window.performance.now();
+    results = await model.predict(images);
+    await results.data();
+    results.dispose();
+    let endTime = window.performance.now();
+    times.push((endTime - startTime) / 1000.0);
 
-  const IMAGES = tf.randomNormal([BATCH_SIZE, ...IMAGE_SIZE]);
-  const LABELS = tf.oneHot(Array(BATCH_SIZE).fill(0), NUM_CLASSES);
-
-  function get_compilation_options() {
-    return {
-      optimizer: tf.train.sgd(0.01),
-      loss: 'categoricalCrossentropy'
-    };
-  };
+    const status = document.getElementById("modelPredictStatus");
+    status.innerText = "model.predict times:\n" + JSON.stringify(times) + ",";
+    await sleep(MS_BETWEEN_ITERATIONS);
+  }
+  console.log(JSON.stringify(times) + ",");
+  await sleep(1000);
+}
 
 
-  async function timeModelPredict(model) {
-    // don't time the jit compilation.
-    console.log("compiling model.predict");
-    await model.predict(IMAGES);
-
-    console.log("benchmarking model.predict");
-    let times = [];
-    for (let i = 0; i < BENCHMARK_ITERATIONS; i++) {
-      let startTime = window.performance.now();
-      await model.predict(IMAGES);
-      let endTime = window.performance.now();
-      times.push(endTime - startTime);
+async function getHistoryResults(history) {
+  // Ensure that all of the values of h.history are set before finishing
+  // the benchmark iteration.
+  const promises = [];
+  const tensors = [];
+  for (const key in history.history) {
+    const valueArray = history.history[key];
+    for (let i = 0; i < valueArray.length; ++i) {
+      if (typeof valueArray[i] !== "number") {
+        const valueScalar = valueArray[i];
+        tensors.push(valueScalar);
+        promises.push(valueScalar.data());
+      }
     }
-    console.log(JSON.stringify(times));
-    await sleep(1000);
+  }
+  const values = await Promise.all(promises);
+  return tensors
+}
+
+
+async function timeModelFit(model, images, labels) {
+  // don't time the jit compilation.
+  console.log("compiling model.fit");
+  const history = await model.fit(images, labels,
+                                  { epochs: 1, batchSize: BATCH_SIZE });
+  const tensors = await getHistoryResults(history);
+  tf.dispose(tensors);
+
+  console.log("benchmarking model.fit");
+  let times = [];
+  for (let i = 0; i < BENCHMARK_ITERATIONS; i++) {
+    let startTime = window.performance.now();
+    const history = await model.fit(images, labels,
+                                    { epochs: 1, batchSize: BATCH_SIZE });
+    const tensors = await getHistoryResults(history);
+    tf.dispose(tensors);
+    let endTime = window.performance.now();
+    times.push((endTime - startTime) / 1000.0);
+
+    const status = document.getElementById("modelFitStatus");
+    status.innerText = "model.fit times:\n" + JSON.stringify(times) + ",";
+    await sleep(MS_BETWEEN_ITERATIONS);
+  }
+  // Log all times in a copy-pastable form if training completes.
+  console.log(JSON.stringify(times) + ",");
+  await sleep(1000);
+}
+
+
+async function timeModelTrainOnBatch(model, images, labels) {
+  // don't time the jit compilation.
+  console.log("compiling model.trainOnBatch");
+  const history = await model.trainOnBatch(images, labels);
+  const tensors = await getHistoryResults(history);
+  tf.dispose(tensors);
+
+  console.log("benchmarking model.trainOnBatch");
+  let times = [];
+  for (let i = 0; i < BENCHMARK_ITERATIONS; i++) {
+    let startTime = window.performance.now();
+    const history = await model.trainOnBatch(images, labels);
+    const tensors = await getHistoryResults(history);
+    let endTime = window.performance.now();
+    tf.dispose(tensors);
+    times.push((endTime - startTime) / 1000.0);
+
+    const status = document.getElementById("modelTrainOnBatchStatus");
+    status.innerText = `model.trainOnBatch ${i+1}/${BENCHMARK_ITERATIONS}:\n${JSON.stringify(times)},`;
+    await sleep(MS_BETWEEN_ITERATIONS);
+  }
+  // Log all times in a copy-pastable form if training completes.
+  console.log(JSON.stringify(times) + ",");
+  await sleep(1000);
+}
+
+
+async function benchmarkModel(model, images, labels, model_name) {
+  if (BENCHMARK_PREDICT) {
+    console.log("Benchmarking " + model_name + ".predict");
+    await timeModelPredict(model, images);
   }
 
-
-  async function timeModelFit(model) {
-    // don't time the jit compilation.
-    console.log("compiling model.fit");
-    await model.fit(IMAGES, LABELS, { epochs: EPOCHS, batchSize: BATCH_SIZE });
-
-    console.log("benchmarking model.fit");
-    let times = [];
-    for (let i = 0; i < BENCHMARK_ITERATIONS; i++) {
-      let startTime = window.performance.now();
-      await model.fit(IMAGES, LABELS, { epochs: EPOCHS, batchSize: BATCH_SIZE });
-      let endTime = window.performance.now();
-      times.push(endTime - startTime);
-    }
-    console.log(JSON.stringify(times));
-    await sleep(1000);
+  if (BENCHMARK_FIT) {
+    console.log("Benhcmarking " + model_name + ".fit");
+    await timeModelFit(model, images, labels);
   }
 
-
-  async function trainSimpleDnn() {
-    console.log("compiling simple dnn");
-    const model = tf.sequential();
-    model.add(tf.layers.flatten({ inputShape: IMAGE_SIZE }));
-    model.add(tf.layers.dense({ units: 128 }));
-    model.add(tf.layers.reLU());
-    model.add(tf.layers.dense({ units: NUM_CLASSES }));
-    model.add(tf.layers.softmax());
-    model.compile(get_compilation_options());
-
-    console.log("benchmarking simple_dnn.predict");
-    await timeModelPredict(model);
-
-    console.log("benhcmarking simple_dnn.fit");
-    await timeModelFit(model);
+  if (BENCHMARK_TRAIN_ON_BATCH) {
+    console.log("Benhcmarking " + model_name + ".trainOnBatch");
+    await timeModelTrainOnBatch(model, images, labels);
   }
+}
 
-  await trainSimpleDnn();
 
-  async function trainMobileNet() {
-    console.log("loading mobilenet");
-    const mobilenet = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
-    mobilenet.compile(get_compilation_options());
-    // mobilenet.summary();
+async function benchmarkMnistDnn() {
+  const image_size = [28, 28, 1];
+  const num_classes = 10;
+  const images = tf.randomNormal([BATCH_SIZE, ...image_size]);
+  const labels = tf.oneHot(Array(BATCH_SIZE).fill(0), num_classes);
 
-    console.log("benchmarking mobilenet.predict");
-    await timeModelPredict(mobilenet);
+  const model = tf.sequential();
+  model.add(tf.layers.flatten({ inputShape: image_size }));
+  model.add(tf.layers.dense({ units: 128 }));
+  model.add(tf.layers.reLU());
+  model.add(tf.layers.dense({ units: num_classes }));
+  model.add(tf.layers.softmax());
+  model.compile(get_compilation_options());
 
-    console.log("benchmarking mobilenet.fit");
-    await timeModelFit(mobilenet);
+  benchmarkModel(model, images, labels, "mnist_dnn");
+}
+
+
+async function benchmarkMobileNet() {
+  const image_size = [224, 224, 3];
+  const num_classes = 1000;
+  const images = tf.randomNormal([BATCH_SIZE, ...image_size]);
+  const labels = tf.oneHot(Array(BATCH_SIZE).fill(0), num_classes);
+
+  console.log("Loading MobileNetV1");
+  const mobilenet = await tf.loadLayersModel(
+    "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json");
+  mobilenet.compile(get_compilation_options());
+
+  benchmarkModel(model, images, labels, "mobilenet");
+}
+
+tf.env().set('WEBGL_FLUSH_THRESHOLD', 1);
+tf.setBackend(TFJS_BACKEND).then(async () => {
+  console.log("TF.js backend:         " + tf.getBackend());
+  console.log("TF.js version:         " + tf.version.tfjs);
+
+  if (MODEL_NAME == "mnist_dnn") {
+    benchmarkMnistDnn();
+  } else if (MODEL_NAME == "mobilenet") {
+    benchmarkMobileNet();
+  } else {
+    throw "MODEL_NAME must be 'mnist_dnn' or 'mobilent', not " + MODEL_NAME;
   }
-
-  // await trainMobileNet();
-
-  console.log("finished.");
-
 });
